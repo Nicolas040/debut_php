2478a23f15a78761b9b72cf7e3c44e827fa8dd90
Nicolas Renard <nrenard29@protonmail.com>
2023-09-15 14:31:46 +0200

    Quelques essais avec PHP.

--- app/index.php
+++ app/index.php
@@ -18,4 +18,32 @@ $Variable = "Une autre variable";
 
 echo $Variable;
 
+$data1 = "Hello";
+$data2 = "planète";
+$data3 = "Mars";
+$data4 = "les terriens";
+
+echo "$data1, $data4. Je viens de la $data2 $data3.";
+
+define('USER','John Doe');
+
+define('tableau', ['client', 4, true, 4.5]);
+echo "<br>";
+var_dump(tableau);
+echo "<br>";
+var_dump(USER);
+
+define('pi',3.141592);
+var_dump(pi*2);
+echo "<br>";
+var_dump($GLOBALS);
+
+$client = "Jane Done";
+$formation = "architecte";
+echo "<br>";
+echo "$client suit une formation d'$formation";
+
+echo "<br>";
+
+
 ?>
